65392da7694eb9018c7ecace8feaead57208a892
diegoarcos7 <arcos.5c@gmail.com>
2021-10-29 14:58:29 -0500

    FontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        fraucens: "'fraucens', serif",
+      }
+    },
   },
   variants: {
     extend: {},
